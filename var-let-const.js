a5c34f25dd481eb881582a6848ce346cc6913297
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-11-25 20:10:01 +0600

    file added

---
+++ var-let-const.js
@@ -0,0 +1,21 @@
+const money = 25;
+const rich = 40 + money;
+// console.log(rich)
+
+let count = 0;
+count += 10;
+// console.log(count)
+
+const numbers = [23, 4, 22, 11];
+numbers.shift()
+// console.log(numbers);
+
+
+const student = {
+    name: 'Asgor Ali',
+    class: 12
+};
+
+student.class = 11;
+console.log(student)
+
