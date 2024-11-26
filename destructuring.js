a5b673c121494991890c5e8133ef7f8f98520eec
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-11-26 19:32:28 +0600

    destructuring contiues

---
+++ destructuring.js
@@ -0,0 +1,52 @@
+const actor = {
+  name: "Ananta",
+  age: 11,
+  phone: "01832148385",
+  money: 12112121,
+};
+
+const { phone, age: boyos } = actor;
+
+// const phone = actor.phone;
+
+// console.log(phone);
+// console.log(boyos);
+// console.log(phone)
+// console.log(phone)
+// console.log(phone)
+// console.log(phone)
+
+// Array Destruction
+const numbers = [45, 99, 22, 121, 2323];
+// const [first, second] = numbers;
+// console.log(first)
+// console.log(second)
+
+// Swapping
+/* let x = 20;
+let y = 1000;
+[x, y] = [y, x];
+console.log(x)
+console.log(y)
+ */
+
+// const serial = [20, 88];
+// const [first , second] = serial;
+// console.log(first)
+// console.log(second)
+
+function doubleThem(a, b) {
+  return [a * 2, b * 2];
+}
+
+// const [first, second]= doubleThem(3, 5)
+// console.log(first)
+// console.log(second)
+
+
+
+
+
+
+
+
